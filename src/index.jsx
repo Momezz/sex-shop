a5b7fc0e05fdc707241252032566bca4148cf158
Julian Gomez <julgomez14@gmail.com>
2023-06-10 23:44:35 -0500

    I add props and js functionality

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import '../src/assets/css/variables.css';
+import '../src/assets/css/normalize.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
